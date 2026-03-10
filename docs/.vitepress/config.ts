36ef0f18dd362739abb09f47b4db638da0b36917
reachedsins <vladelewisky@gmail.com>
2026-03-09 20:30:17 -0600

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/shindo/
   title: "Shindo Life Docs",
   description: "ETEPHYM",
   themeConfig: {
